9a1d1cef63bc3f10f894a1ddea0a0d201b24c444
Jozafath Perez <jozperez@estudiantec.cr>
2023-10-11 23:20:11 -0600

    Creacion de cartones (Logica)
    Co-authored-by: jpozo <jpozoy@users.noreply.github.com>

--- proyecto_bingo/src/CartonBingo.java
+++ proyecto_bingo/src/CartonBingo.java
@@ -11,20 +11,49 @@ public class CartonBingo {
   }
 
   private void generarCarton() {
-
+    for (int columna = 0; columna < 5; columna++) {
+      generarNumeroAleatorio(columna);
+    }
   }
 
   private void generarNumeroAleatorio(int columna) {
-    
+    int limiteInferior = columna * 15 + 1; 
+    int limiteSuperior = (columna + 1) * 15;   
+
+    for (int fila = 0; fila < 5; fila ++) {
+      int numero;
+      do {
+        numero = (int) (Math.random() * (limiteSuperior - limiteInferior + 1)) + limiteInferior; 
+      } while (contieneNumeroEnColumna(columna, numero)); 
+
+      matriz[fila][columna] = numero;
+    }
   }
 
   private boolean contieneNumeroEnColumna(int columna, int numero) {
-    return false;
+    for (int fila = 0; fila < 5; fila++) {
+      if (matriz[fila][columna] == numero) {
+        return true; // El número ya existe en la columna
+      }
+    }
+    return false; // El número no existe en la columna
   }
 
   private String generarIdentificadorUnico() {
     // Implementa la lógica para generar un identificador único
     // Debe ser 3 letras y 3 numeros
-    return "PENE" + (int) (Math.random() * 1000);
+    return "PENE" + (int) (Math.random() * 1000); 
   }
+
+  public String getIdentificador() {
+    return identificador;
+ }
+
+ public int[][] getMatriz() {
+    return matriz;
+}
+
+ public int[][] getMatrizMarcado() {
+    return matrizMarcado;
+}
 }
---
+++ proyecto_bingo/src/PruebaCartonBingo.java
@@ -0,0 +1,36 @@
+
+public class PruebaCartonBingo {
+    public static void main(String[] args) {
+        int cantidadCartones = 5; // Cantidad de cartones a generar
+
+        for (int i = 0; i < cantidadCartones; i++) {
+            CartonBingo carton = new CartonBingo();
+            System.out.println("Identificador del Cartón: " + carton.getIdentificador());
+            mostrarCarton(carton);
+            mostrarMatrizMarcado(carton);
+            System.out.println(); // Espacio en blanco para separar los cartones
+        }
+    }
+
+    // Método para mostrar los números en un cartón
+    private static void mostrarCarton(CartonBingo carton) {
+        int[][] matriz = carton.getMatriz();
+        for (int fila = 0; fila < 5; fila++) {
+            for (int columna = 0; columna < 5; columna++) {
+                System.out.print(matriz[fila][columna] + "\t");
+            }
+            System.out.println(); // Cambio de fila
+        }
+    }
+
+        // Método para mostrar los números en un cartón
+    private static void mostrarMatrizMarcado(CartonBingo carton) {
+        int[][] matrizM = carton.getMatrizMarcado();
+        for (int fila = 0; fila < 5; fila++) {
+            for (int columna = 0; columna < 5; columna++) {
+                System.out.print(matrizM[fila][columna] + "\t");
+            }
+            System.out.println(); // Cambio de fila
+        }
+    }
+}
