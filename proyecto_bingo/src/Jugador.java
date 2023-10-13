952cdaece4d6c53a06c7104be26eb0c24551e59a
Jeff-Pozo <pozojefferson410@gmail.com>
2023-10-12 18:01:05 -0600

    Añadir jugadores a archivo csv

--- proyecto_bingo/src/Jugador.java
+++ proyecto_bingo/src/Jugador.java
@@ -1,3 +1,7 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.opencsv.CSVWriter;
 
 public class Jugador {
   private String nombre;
@@ -8,10 +12,24 @@ public class Jugador {
     // Implementar esta picha, playos, yo no voy hacer nada, ya voy a mimir. post.. Manden porno
   }
 
-  public Jugador(String nombre, String correo, int cedula) {
-    this.nombre = nombre;
-    this.correo = correo;
-    this.cedula = cedula;
+public Jugador(String nombre, String correo, int cedula) {
+  this.nombre = nombre;
+  this.correo = correo;
+  this.cedula = cedula;
+}
+
+public void agregarJugadorACSV() {
+  String csvFile = "Jugadores.csv"; // Nombre del archivo csv
+  try (CSVWriter writer = new CSVWriter(new FileWriter(csvFile, true))) {
+    String[] datos = {
+      this.nombre,
+      this.correo,
+      String.valueOf(this.cedula)
+    };
+    writer.writeNext(datos);
+  } catch (IOException e) {
+    e.printStackTrace();
+    }
   }
 
   // Getters y setters (métodos para acceder y modificar los atributos)
